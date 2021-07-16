02481827af91ce29e0eddcdd7de8fff5c44e8020
shadowekko <fernandogualan@outlook.com>
2021-07-16 11:09:56 +0200

    Modelado BD con JPA

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,8 @@ import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependecy;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
+
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
 	private ComponentDependecy componentDependecy;  //
 	private MyBean myBean;
@@ -43,5 +47,15 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
 		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
+
+		try{
+			//error
+			int value= 10/0;
+			LOGGER.info("Mi valor: "+value);
+		} catch (Exception e ){
+			LOGGER.error("Esto es un error al dividir por cero " +e.getMessage());
+		}
+
+
 	}
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,7 +1,12 @@
 package com.fundamentos.springboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
     //Llamar a otra dependencia dentro de una dependencia ---> Inyeccion de dependecias
+
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
     private MyOperation myOperation;
 
@@ -11,7 +16,9 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al metodo printWithDependency");
         int numero = 1;
+        LOGGER.debug("El numero enviado como parametro a la dependecia operacion es: " + numero);
         System.out.println(myOperation.suma(numero));
         System.out.println("Hola desde la implementacion de un bean con dependencia");
     }
