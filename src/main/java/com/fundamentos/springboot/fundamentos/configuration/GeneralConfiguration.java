3d8bbea935d4de01e85b82dd4ef02ce56febe3ce
shadowekko <fernandogualan@outlook.com>
2021-07-20 10:34:16 +0200

    Uso de anotacion @Value parar apuntar a properties

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -8,25 +8,35 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
-    @Value("${value.name}") //Esta notacion nos permite llamar a los valores que hayamos inicializado previamiente en las properties
 
+    @Value("${value.name}") //Esta notacion nos permite llamar a los valores que hayamos inicializado previamiente en las properties
     private String name;
 
     @Value("${value.apellido}") //Esta notacion nos permite llamar a los valores que hayamos inicializado previamiente en las properties
-
     private String apellido;
 
-
     @Value("${value.random}") //Esta notacion nos permite llamar a los valores que hayamos inicializado previamiente en las properties
-
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
 
     @Bean
     public MyBeanWithProperties function(){
@@ -37,10 +47,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){   //Esta implementacion es una interfaz  //Configuracion dataSource con properties y clases
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
